47fcc9159dce920ff24ae86d5f33a98cc2d8fc2e
abdulthe123 <abdullah.msohail@gmail.com>
2025-08-06 05:11:16 -0400

    My first commit, Days 1 and 2 are completed

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the game of life, band edition")
+city = input("What city did you grow up in?\n")
+name_pet = input("What is a name of one of your pets?\n")
+
+print(city + " " + name_pet)
